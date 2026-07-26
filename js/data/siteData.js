25140af3d202757c44ed0e2b5493f237f81fb8ed
gosunsplease <heehawheehawhee@gmail.com>
2026-07-26 15:11:00 -0600

    Update siteData.js

--- js/data/siteData.js
+++ js/data/siteData.js
@@ -3,8 +3,8 @@ const appointmentLinks = [
   // OWNER QUICK EDITS
   // Edit labels and links below. Add or remove buttons as needed.
   // ===========================================================
-  { enabled: true, label: "Wednesday July 22nd Buyer Appointment Schedule", href: "https://www.signupgenius.com/go/9040449A9AF2EA6FB6-64825476-wednesday#/", external: true },
-  { enabled: true, label: "Thursday July 23rd Buyer Appointment Schedule", href: "https://www.signupgenius.com/go/9040449A9AF2EA6FB6-64825483-thursday#/", external: true }
+  { enabled: true, label: "Wednesday July 29th Buyer Appointment Schedule", href: "https://www.signupgenius.com/go/9040449A9AF2EA6FB6-64894267-wednesday#/", external: true },
+  { enabled: true, label: "Thursday July 30th Buyer Appointment Schedule", href: "https://www.signupgenius.com/go/9040449A9AF2EA6FB6-64894268-thursday#/", external: true }
 ];
 
 const siteData = {
